1b49f3689c7279010886a760099a26296047fd9e
Erin Corcoran <corcorec@bc.edu>
2020-10-20 17:49:06 -0400

    dumb mistake

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const margin = {top: 20, right: 10, bottom: 20, left: 10};
-const width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+const width = 960 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
 
 let coffee = d3.csv('coffee-house-chains.csv', d3.autoType).then(d => {
     console.log(d)
@@ -9,55 +9,65 @@ let coffee = d3.csv('coffee-house-chains.csv', d3.autoType).then(d => {
 // CREATE SVG WITH MARGIN CONVENTION
 
 let svg = d3.select('.chart').append('svg')
-        .attr('width', width)
-        .attr('height', height)
-    .append("g")
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-let names = d.map(d => d.company)
-let stor = d.map(d => d.stores)
-let rev = d.map(d => d.revenue)
 
 // CREATE SCALES
 
 const xScale = d3.scaleBand()
-    .domain(names)
+    .domain(d.map(d => d.company))
     .rangeRound([0, width])
     .paddingInner(0.1)
-
+   
 const yScale = d3.scaleLinear()
-    .domain(stor).nice()
+    .domain([0, d3.max(d, d => d.stores)])
     .range([height, 20])
 
-// CREATE BARS
-let rects = d3.selectAll('.bar')
-    .data(d, function(d) {return d.company})
+
+let rectangles = svg.selectAll('.bars')
+    .data(d)
     .enter()
     .append('rect')
-    .attr('x', (d => xScale(d.company)))
-    .attr('y',  height)
+    .attr('x', d => xScale(d.company))
+    .attr('y', d => yScale(d.stores))
+    .attr('height', d => height - yScale(d.stores))
     .attr('width', xScale.bandwidth())
-    .attr('height', height)
-    .attr('fill', 'blue')
+    .attr('fill', 'pink')
+    
 
+/*
+// CREATE BARS
+let bars = svg.selectAll('rect')
+    .data(d)
+    .enter()
+    .append('rect')
+    .attr('x', d => xScale(d.company))
+    .attr('y', yScale(d.stores))
+    .attr('width', xScale.bandwidth())
+    .attr('height', d => height - yScale(d.stores))
+    .attr('fill', 'blue')
+*/
 // CREATE AXES AND AXIS TITLES
 
-const xAxis = d3.axisBottom()
+let xAxis = d3.axisBottom()
     .scale(xScale)
 
-const yAxis = d3.axisLeft()
+let yAxis = d3.axisLeft()
     .scale(yScale)
     .ticks(10)
 
     // call x axis
-    svg.append("g")
-        .attr("class", "axis x-axis")
-        .attr("transform", `translate(0, ${height})`)
-        .call(xAxis)
+svg.append("g")
+    .attr("class", "axis x-axis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis)
     
     // call y axis 
-    svg.append("g")
-        .attr("class", "axis y-axis")
-        .call(yAxis);
+svg.append("g")
+    .attr("class", "axis y-axis")
+    .call(yAxis);
 
 });
