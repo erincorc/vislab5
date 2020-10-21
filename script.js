b506f57cfec646c95fb1861ff1ef6185813423af
Erin Corcoran <corcorec@bc.edu>
2020-10-20 21:03:28 -0400

    we trying

--- script.js
+++ script.js
@@ -2,83 +2,98 @@ const margin = {top: 20, right: 10, bottom: 20, left: 10};
 const width = 960 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom;
 
-let coffee = d3.csv('coffee-house-chains.csv', d3.autoType).then(d => {
-    console.log(d)
-
-
-// CREATE SVG WITH MARGIN CONVENTION
-
+// CHART INIT -----------------------
 let svg = d3.select('.chart').append('svg')
         .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-// CREATE SCALES
+// CREATE SCALES WITHOUT DOMAINS
 
 const xScale = d3.scaleBand()
-    .domain(d.map(d => d.company))
+//  .domain(d.map(d => d.company))
     .rangeRound([0, width])
-    .paddingInner(0.1)
+    .paddingInner(0.1);
    
 const yScale = d3.scaleLinear()
-    .domain([0, d3.max(d, d => d.stores)])
-    .range([height, 20])
+//  .domain([0, d3.max(d, d => d.stores)])
+    .range([height, 20]);
 
-
-let rectangles = svg.selectAll('.bars')
-    .data(d)
-    .enter()
-    .append('rect')
-    .attr('x', d => xScale(d.company))
-    .attr('y', d => yScale(d.stores))
-    .attr('height', d => height - yScale(d.stores))
-    .attr('width', xScale.bandwidth())
-    .attr('fill', 'pink')
-    
-
-/*
-// CREATE BARS
-let bars = svg.selectAll('rect')
-    .data(d)
-    .enter()
-    .append('rect')
-    .attr('x', d => xScale(d.company))
-    .attr('y', yScale(d.stores))
-    .attr('width', xScale.bandwidth())
-    .attr('height', d => height - yScale(d.stores))
-    .attr('fill', 'blue')
-*/
-// CREATE AXES AND AXIS TITLES
-
-let xAxis = d3.axisBottom()
+const xAxis = d3.axisBottom()
     .scale(xScale)
 
-let yAxis = d3.axisLeft()
+const yAxis = d3.axisLeft()
     .scale(yScale)
-    .tickValues(yScale.ticks().concat(yScale.domain()))
-
-    // call x axis
+    .tickValues(yScale.ticks().concat(yScale.domain()))    
+    
+// call x axis
 svg.append("g")
     .attr("class", "axis x-axis")
     .attr("transform", `translate(0, ${height})`)
-    .call(xAxis)
+    .call(xAxis);
     
     // call y axis 
 svg.append("g")
     .attr("class", "axis y-axis")
-    .call(yAxis)
+    .call(yAxis);
 
 svg.append("text")
     .attr("class", "y-axis-label")
     .attr('x', 20)
     .attr('y', 10)
-    .text("Number of Stores")
+    .text("Number of Stores");
 
 svg.append("text")
     .attr('x', 850)
     .attr('y', 420)
     .text("Chain Names");
-    
-});
+
+//let coffee = d3.csv('coffee-house-chains.csv', d3.autoType).then(d => {
+//    console.log(d)
+
+let type = document.querySelector("#group-by option[value]")
+
+const colorScale = d3.scaleOrdinal(d3.schemeTableau10)
+
+// UPDATE FUNCTION
+function update(d, type) {
+
+    const names = d.map(d => d.company)
+    console.log(names)
+
+    if (type == 'stores') {
+
+        console.log('in stores')
+        const rectangles = svg.selectAll('.bars')
+            .data(d);
+            
+        rectangles.enter() 
+            .append('rect')
+            .attr('x', d => xScale(d.company))
+            .attr('y', d => yScale(d[type]))
+            .attr('height', d => height - yScale(d[type]))
+            .attr('width', xScale.bandwidth())
+            .attr('fill', d => colorScale(d));
+    }
+  
+    else { // type == 'revenue
+        console.log('in revenue')
+        const rectangles = svg.selectAll('.bars')
+            .data(d)
+            .enter()
+            .append('rect')
+            .attr('x', d => xScale(d.company))
+            .attr('y', d => yScale(d[type]))
+            .attr('height', d => height - yScale(d[type]))
+            .attr('width', xScale.bandwidth())
+            .attr('fill', d => colorScale(d));
+    };
+
+
+    // Implement the enter-update-exist sequence
+
+    // Update axes and axis title
+
+};
